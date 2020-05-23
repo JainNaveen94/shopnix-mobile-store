6edca4fd23dd42b74f08cc5029226e51162376d2
Naveen Jain <47914199+JainNaveen94@users.noreply.github.com>
2020-05-24 01:42:27 +0530

    dsigning search filter done

--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -5,14 +5,27 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 import WithErrorHandler from "../../hoc/WithErrorHandler/WithErrorHandler";
 
 import Product from "../../components/Product/Product";
+import ProductSearchFilter from "../../components/Product/ProductSearchFilter/ProductSearchFilter";
 
 import axios from "../../services/axios/axios-product";
 
 class Products extends Component {
-  state = {
-    products: [],
-    loading: false,
-  };
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+      loading: false,
+      currentPage: 1,
+      productPerPage: 3,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id),
+    });
+  }
 
   componentDidMount = () => {
     this.setState({ loading: true });
@@ -34,16 +47,59 @@ class Products extends Component {
   };
 
   render() {
-    let productList = this.state.products.map((product) => {
+    const { products, currentPage, productPerPage } = this.state;
+
+    // Logic for displaying products
+    const indexOfLastProduct = currentPage * productPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productPerPage;
+    const currentProducts = products.slice(
+      indexOfFirstProduct,
+      indexOfLastProduct
+    );
+
+    let productList = currentProducts.map((product, key) => {
       return <Product key={product.id} productDetail={product} />;
+    });
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(products.length / productPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map((number) => {
+      if (number === this.state.currentPage) {
+        return (
+          <>
+            <button
+              className={productsCSS.Active}
+              key={number}
+              id={number}
+              onClick={this.handleClick}
+            >
+              {number}
+            </button>
+          </>
+        );
+      } else {
+        return (
+          <>
+            <button key={number} id={number} onClick={this.handleClick}>
+              {number}
+            </button>
+          </>
+        );
+      }
     });
 
     let productsDisplay = (
       <>
         <div className={productsCSS.Title}>
-          <h1>Product List</h1>
+          {/* <h1>Product List</h1> */}
+          <ProductSearchFilter />
         </div>
         <div className={productsCSS.Products}>{productList}</div>
+        <div className={productsCSS.Pagination}>{renderPageNumbers}</div>
       </>
     );
 
