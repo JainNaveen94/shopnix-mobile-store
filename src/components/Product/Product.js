003c3936ae76ea7cf525a0cca7d1c996f417a42a
Naveen Jain <47914199+JainNaveen94@users.noreply.github.com>
2020-05-23 20:42:48 +0530

    product-page-completed

---
+++ src/components/Product/Product.js
@@ -0,0 +1,44 @@
+import React from "react";
+import productCSS from "./Product.css";
+
+const product = (props) => {
+  let specification = { ...props.productDetail };
+
+  delete specification.model;
+  delete specification.quantity;
+  delete specification.price;
+  delete specification.id;
+
+  let tableRow = Object.keys(specification).map((key) => {
+    return (
+      <tr key={key}>
+        <td style={{textTransform: "uppercase"}}>{key}</td>
+        <td>{specification[key]}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div className={productCSS.Product}>
+      <h1>{props.productDetail.model}</h1>
+      <p className={productCSS.Price}>Rs.{props.productDetail.price}</p>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Specs</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>{tableRow}</tbody>
+        </table>
+      </div>
+      <div>
+        <button>View</button>
+        <button>AddToCart</button>
+      </div>
+    </div>
+  );
+};
+
+export default product;
