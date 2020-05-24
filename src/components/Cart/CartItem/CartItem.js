a535c60225bd089a0bad444b878812165da662f8
Naveen Jain <47914199+JainNaveen94@users.noreply.github.com>
2020-05-25 00:08:59 +0530

    checkout-component-done

--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -10,28 +10,48 @@ import cartItemCSS from "./CartItem.css";
 
 const cartItem = (props) => {
   return (
-    // <div className={cartItemCSS.CartItem}>
     <tr>
-      <td style={{textAlign: "center"}}>
+      <td style={{ textAlign: "center" }}>
         <img
-          src="https://www.bsetechnology.com/wp-content/uploads/2018/07/iPhoneX-silver-1.jpg"
+          src={props.item.url}
           width="50px"
           height="50px"
           alt="Not Available"
         />
       </td>
-      <td style={{textAlign: "center"}}>Iphone X</td>
-      <td style={{textAlign: "center"}}>
-        <img src={addLogo} width="15px" height="25px" alt="Not Available" />
-        <span className={cartItemCSS.Quantity}>10</span>
-        <img src={removeLogo} width="15px" height="25px" alt="Not Available" />
+      <td style={{ textAlign: "center" }}>{props.item.model}</td>
+      <td style={{ textAlign: "center" }}>
+        <img
+          onClick={() => props.decrementQuantityClicked(props.item)}
+          style={{ cursor: "pointer" }}
+          src={removeLogo}
+          width="15px"
+          height="25px"
+          alt="Not Available"
+        />
+
+        <span className={cartItemCSS.Quantity}>{props.item.quantity}</span>
+        <img
+          onClick={() => props.incrementQuantityClicked(props.item)}
+          style={{ cursor: "pointer" }}
+          src={addLogo}
+          width="15px"
+          height="25px"
+          alt="Not Available"
+        />
       </td>
-      <td style={{textAlign: "center"}}>Rs.10000</td>
-      <td style={{textAlign: "center"}}>
-        <img src={deleteLogo} width="25px" height="25px" alt="Not Available" />
+      <td style={{ textAlign: "center" }}>Rs.{props.item.price}</td>
+      <td style={{ textAlign: "center" }}>
+        <img
+          onClick={() => props.deleteItemClicked(props.item.id)}
+          style={{ cursor: "pointer" }}
+          src={deleteLogo}
+          width="25px"
+          height="25px"
+          alt="Not Available"
+        />
       </td>
     </tr>
-    // </div>
   );
 };
 
--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -4,9 +4,199 @@ import cartCSS from "./Cart.css";
 import CartItem from "../../components/Cart/CartItem/CartItem";
 import purchaseLogo from "../../assets/icons/purchase.svg";
 
+import Spinner from "../../components/UI/Spinner/Spinner";
+import Model from "../../components/UI/Model/Model";
+
+import axios from "../../services/axios/axios-product";
+
 class Cart extends Component {
+  state = {
+    cartItems: [],
+    loading: false,
+    showModel: false,
+    modelMessage: "",
+  };
+
+  componentDidMount = () => {
+    this.setState({
+      loading: true,
+    });
+    axios
+      .get("/cart.json")
+      .then((response) => {
+        const cartItems = [];
+        for (let key in response.data) {
+          cartItems.push({
+            ...response.data[key],
+            id: key,
+          });
+        }
+        this.setState({
+          cartItems: cartItems,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          cartItems: [],
+          loading: false,
+        });
+      });
+  };
+
+  calculateCartPrice = () => {
+    let totalPrice = 0;
+    const cartItems = [...this.state.cartItems];
+    cartItems.forEach((item) => {
+      totalPrice = totalPrice + item.quantity * item.price;
+    });
+    return totalPrice;
+  };
+
+  deleteItemFromCartHandler = (id) => {
+    this.setState({
+      loading: true,
+    });
+    axios
+      .delete("/cart/" + id + ".json")
+      .then((response) => {
+        const cartItems = [...this.state.cartItems];
+        cartItems.splice(
+          cartItems.findIndex((item) => item.id === id),
+          1
+        );
+        this.setState({
+          loading: false,
+          cartItems: cartItems,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+        });
+      });
+  };
+
+  incrementQuantityHandler = (item) => {
+    axios
+      .get("/products/" + item.productId + ".json")
+      .then((response) => {
+        if (response.data.quantity > item.quantity) {
+          this.incrementQuantity(item);
+        } else {
+          this.setState({
+            showModel: true,
+            modelMessage: `Only ${item.quantity} quantity is Available for this Product`,
+          });
+        }
+      })
+      .catch((error) => {
+        this.setState({
+          showModel: true,
+          modelMessage: `Server Issue While Fetching Order`,
+        });
+      });
+  };
+
+  decrementQuantityHandler = (item) => {
+    if (item.quantity === 1) {
+      this.deleteItemFromCartHandler(item.id);
+    } else if (item.quantity > 1) {
+      this.decrementQuantity(item);
+    } else {
+      this.setState({
+        showModel: true,
+        modelMessage: "Invalid Quanity",
+      });
+    }
+  };
+
+  decrementQuantity = (item) => {
+    item.quantity = item.quantity - 1;
+    axios
+      .patch("/cart/" + item.id + ".json", { quantity: item.quantity })
+      .then((response) => {
+        const cartItems = [...this.state.cartItems];
+        const index = cartItems.findIndex((itm) => itm.id === item.id);
+        cartItems[index].quantity = item.quantity;
+        this.setState({
+          cartItems: cartItems,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          showModel: true,
+          modelMessage: `Server Issue While Decrement Order`,
+        });
+      });
+  };
+
+  incrementQuantity = (item) => {
+    item.quantity = item.quantity + 1;
+    axios
+      .patch("/cart/" + item.id + ".json", { quantity: item.quantity })
+      .then((response) => {
+        const cartItems = [...this.state.cartItems];
+        const index = cartItems.findIndex((itm) => itm.id === item.id);
+        cartItems[index].quantity = item.quantity;
+        this.setState({
+          cartItems: cartItems,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          showModel: true,
+          modelMessage: `Server Issue While Incrementing Order`,
+        });
+      });
+  };
+
+  modelCloseHandler = () => {
+    this.setState({
+      showModel: false,
+      modelMessage: "",
+    });
+  };
+
+  purchaseOrderHandler = () => {
+    // axios
+    //   .delete("/cart.json")
+    //   .then((response) => {
+    //     const cartItems = [...this.state.cartItems];
+    //     cartItems.splice(
+    //       cartItems.findIndex((item) => item.id === id),
+    //       1
+    //     );
+    //     this.setState({
+    //       loading: false,
+    //       cartItems: cartItems,
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     this.setState({
+    //       loading: false,
+    //     });
+    //   });
+  }
+
   render() {
-    return (
+    let cartItems = this.state.cartItems.map((item) => {
+      return (
+        <CartItem
+          key={item.id}
+          item={item}
+          deleteItemClicked={(id) => this.deleteItemFromCartHandler(id)}
+          incrementQuantityClicked={(item) =>
+            this.incrementQuantityHandler(item)
+          }
+          decrementQuantityClicked={(item) =>
+            this.decrementQuantityHandler(item)
+          }
+        />
+      );
+    });
+
+    let cartDisplayed = (
       <div className={cartCSS.Cart}>
         <div>
           <h1>Cart</h1>
@@ -23,28 +213,15 @@ class Cart extends Component {
               </tr>
             </thead>
             <tbody>
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
-              <CartItem />
+              {cartItems}
               <tr className={cartCSS.LastRow}>
                 <td colSpan="2" className={cartCSS.PriceCol}>
-                  <strong>Total Price : </strong>Rs.100000
+                  <strong>Total Price : </strong>Rs.{this.calculateCartPrice()}
                 </td>
                 <td colSpan="3" className={cartCSS.PurchaseCol}>
                   <img
+                    onClick={() => this.purchaseOrderHandler()}
+                    style={{ cursor: "pointer" }}
                     src={purchaseLogo}
                     width="50px"
                     height="50px"
@@ -56,6 +233,22 @@ class Cart extends Component {
           </table>
         </div>
       </div>
+    );
+
+    if (this.state.loading) {
+      cartDisplayed = <Spinner />;
+    }
+
+    return (
+      <>
+        <Model
+          show={this.state.showModel}
+          modelClose={() => this.modelCloseHandler()}
+        >
+          <h3>{this.state.modelMessage}</h3>
+        </Model>
+        {cartDisplayed}
+      </>
     );
   }
 }
