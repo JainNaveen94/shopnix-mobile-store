4cfe6960e1e73e3c011dc9f2ef1dfbc06eea86e6
Naveen Jain <47914199+JainNaveen94@users.noreply.github.com>
2020-05-24 13:38:08 +0530

    cart-folder structure done

---
+++ src/components/Cart/CartItem/CartItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const cartItem = (props) => {
+    return (
+        <></>
+    )
+}
+
+export default cartItem;
---
+++ src/containers/Cart/Cart.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import cartCSS from "./Cart.css";
+
+class Cart extends Component {
+  render() {
+    return <div className={cartCSS.Cart}></div>;
+  }
+}
+
+export default Cart;
